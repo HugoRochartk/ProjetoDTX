117133d0dd04cabebd3a0299a40325eae89a652b
Hugo Rocha <hugorochartk@gmail.com>
2024-06-13 05:22:28 +0100

    upd

---
+++ count_labels.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+
+
+df = pd.read_csv('C:\\Users\\HugoRocha\\Desktop\\ProjetoDTX\dataframes\\multi_labeled_dataframe.csv', sep=';')
+
+
+counter = {}
+
+
+for s in list(df['possible_labels']):
+
+    splitted = s.split('/')
+    for label in splitted:
+        if label not in counter:
+            counter[label] = 1
+        else:
+            counter[label] += 1
+
+
+print(sorted(list(counter.items()), key = lambda x: -x[1]))
+
+
---
+++ get_highlights_nr.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+df = pd.read_csv('C:\\Users\\HugoRocha\\Desktop\\ProjetoDTX\dataframes\\binary_labeled_dataframe.csv', sep=';')
+
+print(df['label'])
+
+h_c = 0
+nh_c = 0
+for elem in df['label']:
+    if elem == 'Highlight':
+        h_c += 1
+    else:
+        nh_c += 1
+
+
+print(f'Highlights: {h_c}\nNon-Highlights: {nh_c}')
